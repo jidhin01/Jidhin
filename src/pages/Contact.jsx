0dfa1ef115efdfbcf6ce423d0d853ae7ab321a0c
TEN <PRO@TEN.local>
2025-08-01 19:10:09 +0530

    ok for all screen

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,16 +6,16 @@ import { ContactFormExample } from "../components/Email";
 // Social Icons Component
 const Icons = {
   linkedin: () => (
-    <FaLinkedin className="text-black group-hover:fill-sky-600 transition-all duration-500 ease-in-out" size={60} />
+    <FaLinkedin className="text-black group-hover:fill-sky-600 transition-all duration-500 ease-in-out" size={86} />
   ),
   github: () => (
-    <FaGithub className="text-black group-hover:fill-gray-950 transition-all duration-500 ease-in-out" size={60} />
+    <FaGithub className="text-black group-hover:fill-gray-950 transition-all duration-500 ease-in-out" size={86} />
   ),
   StackOverflow: () => (
-    <FaStackOverflow className="text-black group-hover:fill-orange-500 transition-all duration-500 ease-in-out" size={60} />
+    <FaStackOverflow className="text-black group-hover:fill-orange-500 transition-all duration-500 ease-in-out" size={86} />
   ),
   Twitter: () => (
-    <FaSquareXTwitter className="text-black group-hover:fill-blue-500 transition-all duration-500 ease-in-out" size={60} />
+    <FaSquareXTwitter className="text-black group-hover:fill-gray-950 transition-all duration-500 ease-in-out" size={86} />
   ),
 };
 
@@ -30,6 +30,7 @@ export function Contact() {
       </div>
 
       {/* Right: Social Links */}
+      
       <div className="w-full md:w-1/2 flex flex-col items-center justify-center">
         <section className="grid place-content-center gap-6 py-8 text-black">
           <div className="group flex flex-col sm:flex-row items-center justify-center gap-4">
@@ -37,16 +38,16 @@ export function Contact() {
             <FlipLink href="https://www.linkedin.com/in/jidhin-t/">Linkedin</FlipLink>
           </div>
           <div className="group flex flex-col sm:flex-row items-center justify-center gap-4">
-            <Icons.github />
             <FlipLink href="https://github.com/jidhin01">Github</FlipLink>
+            <Icons.github />
           </div>
           <div className="group flex flex-col sm:flex-row items-center justify-center gap-4">
             <Icons.StackOverflow />
             <FlipLink href="https://stackoverflow.com/users/27804742/jidhin">Stack</FlipLink>
           </div>
           <div className="group flex flex-col sm:flex-row items-center justify-center gap-4">
-            <Icons.Twitter />
             <FlipLink href="https://x.com/Jidhin01">Twitter</FlipLink>
+            <Icons.Twitter />
           </div>
         </section>
       </div>
